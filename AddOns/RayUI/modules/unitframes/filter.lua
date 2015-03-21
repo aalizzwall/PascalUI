c324384418df90db3a3219c4d25b28a48ab32f9e
Pascal <pascal@aitc.com.tw>
2015-03-21 19:09:26 +0800

    remove RayUI

--- AddOns/RayUI/modules/unitframes/filter.lua
+++
@@ -1,9 +0,0 @@
-local R, L, P = unpack(select(2, ...)) --Import: Engine, Locales, ProfileDB, local
-local UF = R:GetModule("UnitFrames")
-
-UF.PvEMeleeBossDebuffs = {
-}
-UF.PvECasterBossDebuffs = {
-}
-UF.PvETankBossDebuffs = {
-}
